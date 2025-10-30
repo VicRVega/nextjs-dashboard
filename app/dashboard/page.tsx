3d52a312c2452c5f2702ae0675b10eb22d13104a
VicRVega <vicramirez@csumb.edu@csumb.edu>
2025-10-30 15:13:03 -0700

    Waterfalls

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,8 +10,8 @@ import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data
 
 export default async function Page() {
     const revenue = await fetchRevenue();
-    const latestInvoices = await fetchLatestInvoices(); 
-    const { numberOfInvoices, numberOfCustomers, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
+    const latestInvoices = await fetchLatestInvoices(); // wait for fetchRevenue() to finish
+    const { numberOfInvoices, numberOfCustomers, totalPaidInvoices, totalPendingInvoices } = await fetchCardData(); // wait for fetchLatestInvoices() to finish
 
   return (
     <main>
@@ -35,3 +35,18 @@ export default async function Page() {
 }
 
 //The code above is intentionally commented out. We will now begin to example each piece.
+
+
+//However... there are two things you need to be aware of:
+    //1. The data requests are unintentionally blocking each other, creating a request waterfall.
+    //2. By default, Next.js prerenders routes to improve performance, this is called Static Rendering. So if your data changes, it won't be reflected in your dashboard.
+    //Let's discuss number 1 in this chapter, then look into detail at number 2 in the next chapter(ch 8).
+
+//1. Waterfall: 
+/*For example, we need to wait for fetchRevenue() to execute before fetchLatestInvoices() can start running, and so on.
+This pattern is not necessarily bad. 
+There may be cases where you want waterfalls because you want a condition to be satisfied before you make the next request. 
+For example, you might want to fetch a user's ID and profile information first. 
+Once you have the ID, you might then proceed to fetch their list of friends. 
+In this case, each request is contingent on the data returned from the previous request.
+However, this behavior can also be unintentional and impact performance.*/
