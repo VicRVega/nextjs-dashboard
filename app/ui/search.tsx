4772866a5463e2177f8b6a7f58a4df43068e82e6
VicRVega <vicramirez@csumb.edu@csumb.edu>
2025-11-02 12:53:18 -0800

    Ch 11 Capture User's Input

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,12 @@
-'use client';
+'use client'; //client component, means you can use event listeners and hooks
+//<input> this is the search input 
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  function handleSearch(term:string){
+    console.log(term);
+  }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,8 +15,15 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
 }
+//Ch 10: A. capture the user's input in a search box
+/*
+1. Create a new handleSearch function, 
+2. and add an onChange listener to the <input> element. 
+3. onChange will invoke handleSearch whenever the input value changes.
+*/
