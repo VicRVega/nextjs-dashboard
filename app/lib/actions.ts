c054f0b11d58fa6f1a80d3283af5012a9251b51a
VicRVega <vicramirez@csumb.edu@csumb.edu>
2025-11-04 09:18:57 -0800

    Server-Side Validation (useActionState) Customer Name

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,45 +7,74 @@ import postgres from 'postgres'; //step 5 inserting in db
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' }); //step 5
 
-//step 4: create a schema to validate and parse the form data
+//Ch 12: step 4: create a schema to validate and parse the form data
+//Ch 14:  use Zod to validate form data. Update your FormSchema
 const FormSchema = z.object({
     id: z.string(), 
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.', //Ch 14: Server-Side Validation 
+    }),
+    amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }), //ch14
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.', //ch 14
+
+    }),
     date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-//step 4 pass your rawFormData to CreateInvoice to validate the types:
+//ch 14
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+//Ch 12: step 4 pass your rawFormData to CreateInvoice to validate the types:
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Validate form fields using Zod
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-    //step 4 store values in cents and create new dates 
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
 
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+    // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+   
+  // Insert data into the database
     try {
-      //step 5 create sql query to insert new invoice
       await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
       `;
     } catch (error) {
+      // If a database error occurs, return a more specific error.
       //we'll also log the error to the console for now
-      console.error(error);
+      //console.error(error);
       return {
         message: 'Database Error: Failed to Create Invoice.'
       };
     }
-
-    revalidatePath('/dashboard/invoices'); //step 6
-    redirect('/dashboard/invoices'); //step 6
+    
+    // Revalidate the cache for the invoices page and redirect the user.
+    revalidatePath('/dashboard/invoices'); 
+    redirect('/dashboard/invoices'); 
 
 }
 
@@ -80,9 +109,26 @@ try{
   redirect('/dashboard/invoices');
 }
 
-//new action deleteInvoice
+//Ch 12 new action deleteInvoice
 export async function deleteInvoice(id: string) {
 
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
+
+//CH14 ServerSide Validation 
+/*
+customerId - Zod already throws an error if the customer field is empty as it expects a type string. But let's add a friendly message if the user doesn't select a customer.
+amount - Since you are coercing the amount type from string to number, it'll default to zero if the string is empty. Let's tell Zod we always want the amount greater than 0 with the .gt() function.
+status - Zod already throws an error if the status field is empty as it expects "pending" or "paid". Let's also add a friendly message if the user doesn't select a status.
+
+Next, update your createInvoice action to accept two parameters - prevState and formData:
+
+formData - same as before.
+prevState - contains the state passed from the useActionState hook. You won't be using it in the action in this example, but it's a required prop.
+Then, change the Zod parse() function to safeParse():
+
+safeParse() will return an object containing either a success or error field. This will help handle validation more gracefully without having put this logic inside the try/catch block.
+
+Before sending the information to your database, check if the form fields were validated correctly with a conditional:
+*/
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,3 +1,5 @@
+'use client'; //ch 14
+
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
@@ -7,12 +9,15 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-// Ch 12. The Form to create a new invoice (step1) 
-import { createInvoice } from '@/app/lib/actions';
+import { createInvoice, State } from '@/app/lib/actions'; //ch12 Form to create new invoice (step1)
+import { useActionState } from 'react'; //ch 14 Sever-side form validation
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const initialState: State = { message: null, errors: {} };
+  const [state, formAction] = useActionState(createInvoice, initialState);
+
   return (
-    <form action={createInvoice}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -25,7 +30,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
-              required
+              //required
+              aria-describery="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -37,6 +43,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.customerId &&
+              state.errors.customerId.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
           </div>
         </div>
 
@@ -54,7 +68,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-                required
+                //required
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -74,7 +88,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   name="status"
                   type="radio"
                   value="pending"
-                  required
+                  //required
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                 />
                 <label
@@ -115,3 +129,16 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
     </form>
   );
 }
+
+//ch14 
+//Back in the create-form.tsx component, you can access the errors using the form state.
+
+//Add a ternary operator that checks for each specific error. For example, after the customer's field, you can add:
+
+/*
+In the code above, you're also adding the following aria labels:
+
+aria-describedby="customer-error": This establishes a relationship between the select element and the error message container. It indicates that the container with id="customer-error" describes the select element. Screen readers will read this description when the user interacts with the select box to notify them of errors.
+id="customer-error": This id attribute uniquely identifies the HTML element that holds the error message for the select input. This is necessary for aria-describedby to establish the relationship.
+aria-live="polite": The screen reader should politely notify the user when the error inside the div is updated. When the content changes (e.g. when a user corrects an error), the screen reader will announce these changes, but only when the user is idle so as not to interrupt them.
+*/
